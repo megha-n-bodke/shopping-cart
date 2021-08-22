3726f896fc680ecd1de3b2d74d25a77d06315444
Megha Bodke <megha.n.bodke@gmail.com>
2021-08-22 17:44:25 +0530

    Seperating Header Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import OurAdvantages from './Components/HomePageEssentials/OurAdvantages';
 import HighQuality from './Components/HomePageEssentials/HighQuality';
-import ContactUs from './Components/Utils/ContactUs';
-import Footer from './Components/Utils/Footer';
-import Header from './Components/Utils/Header';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
 import LoginPage from './Components/Login/LoginPage';
 import{Route,Switch} from 'react-router-dom'
 
