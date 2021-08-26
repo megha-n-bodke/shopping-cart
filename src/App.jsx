b61cb0e0e074745626aff594032395ed15a949a9
Megha Bodke <megha.n.bodke@gmail.com>
2021-08-26 11:42:47 +0530

    Routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import HighQuality from './Components/HomePageEssentials/HighQuality';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import LoginPage from './Components/Login/LoginPage';
-import{Route,Switch} from 'react-router-dom'
+import{BrowserRouter, Route,Switch} from 'react-router-dom'
 import Home from './pages/home/Home';
 import Products from './pages/products/Products';
 import MyCart from './pages/my-cart/MyCart';
@@ -17,19 +17,11 @@ import Blog from './pages/blog/Blog';
 function App() {
   return (
     <div>
-    <switch>
     <Header/>
-    <Switch>
-      <Route exact path = "/" component = {Home} />
-      <Route exact path = "/login" component = {LoginPage} />
-      <Route exact path = "/products" component = {Products} />
-      <Route exact path = "/my-cart" component = {MyCart} />
-      <Route exact path = "/my-wish-list" component = {MyWishList} />
-      <Route exact path = "/albums" component = {Albums} />
-      <Route exact path = "/blog" component = {Blog} />
-    </Switch>
+    <BrowserRouter>
+
+    </BrowserRouter>
     <Footer/>
-    </switch>
     </div>
   );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,34 +1,59 @@
 import React from 'react';
 import './Utils.css'
+import Home from '../../pages/home/Home';
 import { HiOutlineUser } from "react-icons/hi";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-import { Link } from 'react-router-dom';
-
-
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Products from '../../pages/products/Products';
+import Albums from '../../pages/albums/Albums';
+import MyCart from '../../pages/my-cart/MyCart';
+import LoginPage from '../Login/LoginPage';
+import MyWishList from '../../pages/my-wish-list/MyWishList';
+import Blog from '../../pages/blog/Blog';
 
 
 const Header = () => {
     return (
         <>
+            <Router>
             <div className="HeaderContainer">
                 <div>Logo</div>
                 <div className="MenuLink">
                     <ul>
                         <li>
-                            Home
+                            <Link to="/">Home</Link>
                         </li>
                         <li>
-                            Products
+                            <Link to="/products">Products</Link>
                         </li>
-                        <li>Albums</li>
-                        <li>Blogs</li>
+                        <li>
+                            <Link to="/albums">Album</Link>
+                        </li>
+                        <li>
+                            <Link to="/blog">Blog</Link>
+                        </li>
                         <li><AiOutlineShoppingCart /></li>
-                        <li data-toggle="popover" title="Login" title= "myCart" data-content="megha bodke"><HiOutlineUser />
-                        
+                        <li data-toggle="popover" title="Login" title="myCart" data-content="megha bodke"><HiOutlineUser />
                         </li>
                     </ul>
                 </div>
             </div>
+
+            <Switch>
+      <Route exact path = "/" component = {Home} />
+      <Route exact path = "/login" component = {LoginPage} />
+      <Route exact path = "/products" component = {Products} />
+      <Route exact path = "/my-cart" component = {MyCart} />
+      <Route exact path = "/my-wish-list" component = {MyWishList} />
+      <Route exact path = "/albums" component = {Albums} />
+      <Route exact path = "/blog" component = {Blog} />
+        </Switch>
+            </Router>
         </>
     );
 }
